eb4d4eb9717830a0659fcfe64ad680b1e12c365d
Mirza Twhidul Imran <towhidd@users.noreply.github.com>
2015-07-28 11:18:14 +0600

    content-single-product.php

---
+++ content-single-product.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The template for displaying product content in the single-product.php template
+ *
+ * Override this template by copying it to yourtheme/woocommerce/content-single-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+
+<?php
+	/**
+	 * woocommerce_before_single_product hook
+	 *
+	 * @hooked wc_print_notices - 10
+	 */
+	 do_action( 'woocommerce_before_single_product' );
+
+	 if ( post_password_required() ) {
+	 	echo get_the_password_form();
+	 	return;
+	 }
+?>
+
+<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php
+		/**
+		 * woocommerce_before_single_product_summary hook
+		 *
+		 * @hooked woocommerce_show_product_sale_flash - 10
+		 * @hooked woocommerce_show_product_images - 20
+		 */
+		do_action( 'woocommerce_before_single_product_summary' );
+	?>
+
+	<div class="summary entry-summary">
+
+		<?php
+			/**
+			 * woocommerce_single_product_summary hook
+			 *
+			 * @hooked woocommerce_template_single_title - 5
+			 * @hooked woocommerce_template_single_rating - 10
+			 * @hooked woocommerce_template_single_price - 10
+			 * @hooked woocommerce_template_single_excerpt - 20
+			 * @hooked woocommerce_template_single_add_to_cart - 30
+			 * @hooked woocommerce_template_single_meta - 40
+			 * @hooked woocommerce_template_single_sharing - 50
+			 */
+			do_action( 'woocommerce_single_product_summary' );
+		?>
+
+	</div><!-- .summary -->
+	<?php 
+		
+		/*This coustomization was created by TOWHID	*/
+		
+		global $product;
+
+		$author     = get_user_by( 'id', $product->post->post_author );
+    	$store_info = dokan_get_store_info( $author->ID );
+		if ( !empty( $store_info['store_name'] ) ) { ?>
+                <span class="seller_name">
+                    <p>Seller Name: <?php printf( '<a href="%s">%s</a>', dokan_get_store_url( $author->ID ), $author->display_name ); ?></p>
+                	<p>Seller Phone: <?php echo $store_info['phone']; ?></p>
+                </span>
+ 		<?php } ?>
+	<?php
+		/**
+		 * woocommerce_after_single_product_summary hook
+		 *
+		 * @hooked woocommerce_output_product_data_tabs - 10
+		 * @hooked woocommerce_upsell_display - 15
+		 * @hooked woocommerce_output_related_products - 20
+		 */
+		do_action( 'woocommerce_after_single_product_summary' );
+
+
+	?>
+
+	
+
+	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+
+</div><!-- #product-<?php the_ID(); ?> -->
+
+<?php do_action( 'woocommerce_after_single_product' ); ?>
